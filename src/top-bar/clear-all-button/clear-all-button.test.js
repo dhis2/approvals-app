0993bd443a908dd1eba3fcb5ad2e5ca8d35e63a3
Hendrik de Graaf <hendrik@dhis2.org>
2021-06-23 14:07:58 +0200

    test(top-bar): add test for ClearAllButton

---
+++ src/top-bar/clear-all-button/clear-all-button.test.js
@@ -0,0 +1,76 @@
+import { shallow } from 'enzyme'
+import React from 'react'
+import { useSelection } from '../selection/use-selection.js'
+import { ClearAllButton } from './clear-all-button.js'
+
+jest.mock('../selection/use-selection.js', () => ({
+    useSelection: jest.fn(),
+}))
+
+afterEach(() => {
+    jest.resetAllMocks()
+})
+
+describe('<ClearAllButton>', () => {
+    it('renders a button when workflow, period and org-unit are set', () => {
+        useSelection.mockImplementation(() => ({
+            clearAll: jest.fn(),
+            workflow: {
+                id: '123',
+            },
+            period: {
+                code: '20110203',
+            },
+            orgUnit: {
+                id: 'abc',
+            },
+        }))
+        const wrapper = shallow(<ClearAllButton />)
+        const button = wrapper.dive().find({
+            'data-test': 'dhis2-uicore-button',
+        })
+        expect(button).toHaveLength(1)
+    })
+
+    it('renders a button when workflow and period are set', () => {
+        useSelection.mockImplementation(() => ({
+            clearAll: jest.fn(),
+            workflow: {
+                id: '123',
+            },
+            period: {
+                code: '20110203',
+            },
+            orgUnit: {},
+        }))
+        const wrapper = shallow(<ClearAllButton />)
+        const button = wrapper.dive().find({
+            'data-test': 'dhis2-uicore-button',
+        })
+        expect(button).toHaveLength(1)
+    })
+
+    it('renders nothing when only workflow is set', () => {
+        useSelection.mockImplementation(() => ({
+            clearAll: jest.fn(),
+            workflow: {
+                id: '123',
+            },
+            period: {},
+            orgUnit: {},
+        }))
+        const wrapper = shallow(<ClearAllButton />)
+        expect(wrapper.type()).toEqual(null)
+    })
+
+    it('renders nothing when all properties are unset', () => {
+        useSelection.mockImplementation(() => ({
+            clearAll: jest.fn(),
+            workflow: {},
+            period: {},
+            orgUnit: {},
+        }))
+        const wrapper = shallow(<ClearAllButton />)
+        expect(wrapper.type()).toEqual(null)
+    })
+})
