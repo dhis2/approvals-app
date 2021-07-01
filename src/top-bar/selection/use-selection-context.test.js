7cf553dd051933785b067a6bcd4eb33e8f81b666
Hendrik de Graaf <hendrik@dhis2.org>
2021-07-01 12:00:22 +0200

    test(context-selector): fix failing test in use-selection-context.test

--- src/top-bar/selection/use-selection-context.test.js
+++ src/top-bar/selection/use-selection-context.test.js
@@ -1,22 +1,14 @@
 import { act, renderHook } from '@testing-library/react-hooks'
 import React from 'react'
 import { useCurrentUser } from '../../current-user/index.js'
-import { history } from '../../navigation/history.js'
+import { pushStateToHistory } from '../../navigation/push-state-to-history.js'
 import { readQueryParams } from '../../navigation/read-query-params.js'
 import { SelectionProvider } from './selection-provider.js'
 import { useSelectionContext } from './use-selection-context.js'
 
-jest.mock('../../navigation/history.js', () => {
-    const actualHistoryModule = jest.requireActual(
-        '../../navigation/history.js'
-    )
-    return {
-        history: {
-            ...actualHistoryModule.history,
-            push: jest.fn(),
-        },
-    }
-})
+jest.mock('../../navigation/push-state-to-history.js', () => ({
+    pushStateToHistory: jest.fn(),
+}))
 
 jest.mock('../../navigation/read-query-params.js', () => ({
     readQueryParams: jest.fn(),
@@ -30,28 +22,31 @@ afterEach(() => {
     jest.resetAllMocks()
 })
 
-describe('useSelectionContext', () => {
-    const mockWorkflows = [
-        {
-            displayName: 'Workflow a',
-            id: 'i5m0JPw4DQi',
-            periodType: 'Daily',
-        },
-        {
-            displayName: 'Workflow B',
-            id: 'rIUL3hYOjJc',
-            periodType: 'Daily',
-        },
-    ]
+const mockWorkflows = [
+    {
+        displayName: 'Workflow a',
+        id: 'i5m0JPw4DQi',
+        periodType: 'Daily',
+    },
+    {
+        displayName: 'Workflow B',
+        id: 'rIUL3hYOjJc',
+        periodType: 'Daily',
+    },
+]
 
+beforeEach(() => {
+    useCurrentUser.mockImplementation(() => ({
+        dataApprovalWorkflows: mockWorkflows,
+    }))
+})
+
+describe('useSelectionContext', () => {
     const wrapper = ({ children }) => (
         <SelectionProvider>{children}</SelectionProvider>
     )
 
     it('returns the expected properties', () => {
-        useCurrentUser.mockImplementation(() => ({
-            dataApprovalWorkflows: mockWorkflows,
-        }))
         readQueryParams.mockImplementation(() => ({}))
 
         const { result } = renderHook(() => useSelectionContext(), { wrapper })
@@ -72,9 +67,6 @@ describe('useSelectionContext', () => {
     })
 
     it('populates properties from query params', () => {
-        useCurrentUser.mockImplementation(() => ({
-            dataApprovalWorkflows: mockWorkflows,
-        }))
         readQueryParams.mockImplementation(() => ({
             wf: 'rIUL3hYOjJc',
             pe: '20110203',
@@ -85,67 +77,130 @@ describe('useSelectionContext', () => {
         expect(result.current.workflow).toEqual(mockWorkflows[1])
         expect(result.current.period).toEqual(
             expect.objectContaining({
-                id: '20110203',
                 displayName: '2011-02-03',
+                endDate: '2011-02-03',
+                id: '20110203',
+                iso: '20110203',
+                startDate: '2011-02-03',
+                year: 2011,
             })
         )
-        expect(result.current.orgUnit).toEqual({ id: 'abc' })
+        expect(result.current.orgUnit).toEqual({ path: 'abc' })
         // TODO: add tests for dealing with invalid query params
-        // once that has been implemented
+        // once that has been implemented for org-units too.
     })
+    describe('functions returned from the hook update the state and url', () => {
+        it('setOpenedSelect', () => {
+            const mock = jest.fn()
+            readQueryParams.mockImplementation(() => ({}))
+            pushStateToHistory.mockImplementation(mock)
 
-    it('functions returned from the hook update the state and url', () => {
-        const push = jest.fn()
-        useCurrentUser.mockImplementation(() => ({
-            dataApprovalWorkflows: mockWorkflows,
-        }))
-        readQueryParams.mockImplementation(() => ({}))
-        history.push.mockImplementation(push)
+            const { result } = renderHook(() => useSelectionContext(), {
+                wrapper,
+            })
+            // Reset count to 0 because the function is also called on initial render
+            mock.mockClear()
 
-        const { result } = renderHook(() => useSelectionContext(), { wrapper })
-        // Reset count to 0 because the function is also called on initial render
-        push.mockClear()
-
-        const expectedOpenedSelect = 'test'
-        act(() => {
-            result.current.setOpenedSelect(expectedOpenedSelect)
+            const expectedOpenedSelect = 'test'
+            act(() => {
+                result.current.setOpenedSelect(expectedOpenedSelect)
+            })
+            expect(result.current.openedSelect).toEqual(expectedOpenedSelect)
+            // Not captured in URL
+            expect(mock).toHaveBeenCalledTimes(0)
         })
-        expect(result.current.openedSelect).toEqual(expectedOpenedSelect)
-        // Not captured in URL
-        expect(push).toHaveBeenCalledTimes(0)
 
-        const expectedWorkflow = { id: '123' }
-        act(() => {
-            result.current.selectWorkflow(expectedWorkflow)
-        })
-        expect(result.current.workflow).toEqual(expectedWorkflow)
-        expect(push).toHaveBeenCalledTimes(1)
-        push.mockClear()
+        it('selectWorkflow', () => {
+            const mock = jest.fn()
+            readQueryParams.mockImplementation(() => ({}))
+            pushStateToHistory.mockImplementation(mock)
 
-        const expectedPeriod = { id: '20210202' }
-        act(() => {
-            result.current.selectPeriod(expectedPeriod)
-        })
-        expect(result.current.period).toEqual(expectedPeriod)
-        expect(push).toHaveBeenCalledTimes(1)
-        push.mockClear()
+            const { result } = renderHook(() => useSelectionContext(), {
+                wrapper,
+            })
+            // Reset count to 0 because the function is also called on initial render
+            mock.mockClear()
 
-        const expectedOrgUnit = { id: '123' }
-        act(() => {
-            result.current.selectOrgUnit(expectedOrgUnit)
+            const expectedWorkflow = { id: '123' }
+            act(() => {
+                result.current.selectWorkflow(expectedWorkflow)
+            })
+            expect(result.current.workflow).toEqual(expectedWorkflow)
+            expect(mock).toHaveBeenCalledTimes(1)
         })
-        expect(result.current.orgUnit).toEqual(expectedOrgUnit)
-        expect(push).toHaveBeenCalledTimes(1)
-        push.mockClear()
 
-        act(() => {
-            result.current.clearAll()
+        it('selectPeriod', () => {
+            const mock = jest.fn()
+            readQueryParams.mockImplementation(() => ({}))
+            pushStateToHistory.mockImplementation(mock)
+
+            const { result } = renderHook(() => useSelectionContext(), {
+                wrapper,
+            })
+            // Reset count to 0 because the function is also called on initial render
+            mock.mockClear()
+
+            const expectedPeriod = { id: '20210202' }
+            act(() => {
+                result.current.selectPeriod(expectedPeriod)
+            })
+            expect(result.current.period).toEqual(expectedPeriod)
+            expect(mock).toHaveBeenCalledTimes(1)
         })
-        expect(result.current.openedSelect).toEqual('')
-        expect(result.current.workflow).toEqual({})
-        expect(result.current.period).toEqual({})
-        expect(result.current.orgUnit).toEqual({})
-        expect(push).toHaveBeenCalledTimes(1)
-        push.mockClear()
+
+        it('selectOrgUnit', () => {
+            const mock = jest.fn()
+            readQueryParams.mockImplementation(() => ({}))
+            pushStateToHistory.mockImplementation(mock)
+
+            const { result } = renderHook(() => useSelectionContext(), {
+                wrapper,
+            })
+            // Reset count to 0 because the function is also called on initial render
+            mock.mockClear()
+
+            const expectedOrgUnit = { path: '123' }
+            act(() => {
+                result.current.selectOrgUnit(expectedOrgUnit)
+            })
+            expect(result.current.orgUnit).toEqual(expectedOrgUnit)
+            expect(mock).toHaveBeenCalledTimes(1)
+        })
+
+        it('clearAll', () => {
+            const mock = jest.fn()
+            readQueryParams.mockImplementation(() => ({
+                wf: 'i5m0JPw4DQi',
+                pe: '20120402',
+            }))
+            pushStateToHistory.mockImplementation(mock)
+
+            const { result } = renderHook(() => useSelectionContext(), {
+                wrapper,
+            })
+            // Reset count to 0 because the function is also called on initial render
+            mock.mockClear()
+
+            expect(result.current.workflow).toEqual(mockWorkflows[0])
+            expect(result.current.period).toEqual(
+                expect.objectContaining({
+                    displayName: '2012-04-02',
+                    endDate: '2012-04-02',
+                    id: '20120402',
+                    iso: '20120402',
+                    startDate: '2012-04-02',
+                    year: 2012,
+                })
+            )
+
+            act(() => {
+                result.current.clearAll()
+            })
+            expect(result.current.openedSelect).toEqual('')
+            expect(result.current.workflow).toEqual({})
+            expect(result.current.period).toEqual({})
+            expect(result.current.orgUnit).toEqual({})
+            expect(mock).toHaveBeenCalledTimes(1)
+        })
     })
 })
