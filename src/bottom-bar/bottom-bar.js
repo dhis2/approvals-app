8d07b1ac4e1cdb7a1e170be1cfa28a366184e367
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2021-07-07 10:50:28 +0200

    feat(bottom bar actions): add button (/wo functionality)

--- src/bottom-bar/bottom-bar.js
+++ src/bottom-bar/bottom-bar.js
@@ -1,14 +1,69 @@
+import i18n from '@dhis2/d2-i18n'
+import { Button } from '@dhis2/ui'
 import React from 'react'
 import { StatusTag } from '../shared/index.js'
 import { useWorkflowContext } from '../workflow-context/index.js'
+import { BottomBarItem } from './bottom-bar-item.js'
+import styles from './bottom-bar.module.css'
 
 const BottomBar = () => {
-    const { approvalState } = useWorkflowContext()
+    const { approvalStatus } = useWorkflowContext()
+    const {
+        mayAccept,
+        mayApprove,
+        mayUnaccept,
+        mayUnapprove,
+    } = approvalStatus
+
+    const disableApproveBtn = !mayApprove && !mayUnapprove
+    const approve = () => null
+    const accept = () => null
+    const unapprove = () => null
+    const unaccept = () => null
 
     return (
-        <>
-            <StatusTag approvalState={approvalState} />
-        </>
+        <div className={styles.bottomBar}>
+            <BottomBarItem>
+                <StatusTag approvalState={approvalStatus} />
+            </BottomBarItem>
+
+            {(mayApprove || (disableApproveBtn)) && (
+                <BottomBarItem>
+                    <Button
+                        primary
+                        small
+                        disabled={disableApproveBtn}
+                        onClick={approve}
+                    >
+                        {i18n.t('Approve')}
+                    </Button>
+                </BottomBarItem>
+            )}
+
+            {mayAccept && (
+                <BottomBarItem>
+                    <Button small onClick={accept}>
+                        {i18n.t('Accept')}
+                    </Button>
+                </BottomBarItem>
+            )}
+
+            {mayUnapprove && (
+                <BottomBarItem>
+                    <Button small onClick={unapprove}>
+                        {i18n.t('Unapprove')}
+                    </Button>
+                </BottomBarItem>
+            )}
+
+            {mayUnaccept && (
+                <BottomBarItem>
+                    <Button small onClick={unaccept}>
+                        {i18n.t('Unaccept')}
+                    </Button>
+                </BottomBarItem>
+            )}
+        </div>
     )
 }
 
