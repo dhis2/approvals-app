668c0058bdb91c7e7797f62e46acae80d3aad257
Médi-Rémi Hashim <mediremi@gmail.com>
2021-07-14 14:22:24 +0100

    test(data-workspace): add unit tests

--- src/data-workspace/data-workspace.test.js
+++ src/data-workspace/data-workspace.test.js
@@ -1,11 +1,11 @@
-import React from 'react'
 import { shallow } from 'enzyme'
-import { DataWorkspace } from './data-workspace.js'
-import { TitleBar } from './title-bar/index.js'
-import { DataSetNavigation } from './data-set-navigation/index.js'
-import { Display } from './display/index.js'
+import React from 'react'
 import { useWorkflowContext } from '../workflow-context/index.js'
 import { useSelectionParams } from '../workflow-context/use-selection-params.js'
+import { DataSetNavigation } from './data-set-navigation/index.js'
+import { DataWorkspace } from './data-workspace.js'
+import { Display } from './display/index.js'
+import { TitleBar } from './title-bar/index.js'
 
 jest.mock('../workflow-context/index.js', () => ({
     useWorkflowContext: jest.fn(),
@@ -15,18 +15,41 @@ jest.mock('../workflow-context/use-selection-params.js', () => ({
     useSelectionParams: jest.fn(),
 }))
 
-beforeEach(() => {
+const mockDataSets = [
+    {
+        id: 'data-set-1',
+        displayName: 'Data set 1',
+        periodType: 'Monthly',
+    },
+    {
+        id: 'data-set-2',
+        displayName: 'Data set 2',
+        periodType: 'Monthly',
+    },
+]
+
+const mockUseWorkflowContext = (workflow = {}) => {
     useWorkflowContext.mockImplementation(() => ({
         displayName: 'Workflow a',
         id: 'i5m0JPw4DQi',
         periodType: 'Monthly',
         dataSets: [],
-        approvalState: 'APPROVED_HERE'
+        approvalState: 'APPROVED_HERE',
+        ...workflow,
     }))
+}
+
+const mockUseSelectionParams = (params = {}) => {
     useSelectionParams.mockImplementation(() => ({
         pe: '201204',
-        ou: 'ImspTQPwCqd'
+        ou: 'ImspTQPwCqd',
+        ...params,
     }))
+}
+
+beforeEach(() => {
+    mockUseWorkflowContext()
+    mockUseSelectionParams()
 })
 
 afterEach(() => {
@@ -40,5 +63,28 @@ describe('<DataWorkspace>', () => {
         expect(wrapper.find(TitleBar)).toHaveLength(1)
         expect(wrapper.find(DataSetNavigation)).toHaveLength(1)
         expect(wrapper.find(Display)).toHaveLength(1)
+    })
+
+    it('if there is only one data set, select it automatically', () => {
+        const dataSet = mockDataSets[0]
+        mockUseWorkflowContext({
+            dataSets: [dataSet],
+        })
+        const wrapper = shallow(<DataWorkspace />)
+
+        expect(wrapper.find(DataSetNavigation).prop('selected')).toBe(
+            dataSet.id
+        )
+        expect(wrapper.find(Display).prop('dataSetId')).toBe(dataSet.id)
+    })
+
+    it('if there is more than one data set, do not select one automatically', () => {
+        mockUseWorkflowContext({
+            dataSets: mockDataSets,
+        })
+        const wrapper = shallow(<DataWorkspace />)
+
+        expect(wrapper.find(DataSetNavigation).prop('selected')).toBe(null)
+        expect(wrapper.find(Display).prop('dataSetId')).toBe(null)
     })
 })
