4e9f52dd30981f4437c0d6e112e01e8b6ed45ddb
Hendrik de Graaf <HendrikThePendric@users.noreply.github.com>
2021-08-12 14:12:42 +0200

    fix(status-tag): align approval state classification with design specs (#44)

--- src/shared/status-tag/get-tag-display-data.js
+++ src/shared/status-tag/get-tag-display-data.js
@@ -1,57 +1,69 @@
 import i18n from '@dhis2/d2-i18n'
+import { IconBlock16, IconError16 } from '@dhis2/ui'
 import { Approved, Ready, Waiting } from './icons.js'
 
-/*
- * TODO: The current classification logic was discussed with
- * Joe Cooper, but needs to be confirmed by either Lars or Jim.
- * Specifically these cases are not clear:
- * A. Do ACCEPTED_HERE and ACCEPTED_ELSEWHERE fall into "Ready for
- *    approval and accepted"? This doesn't seem to match the webapi docs.
- * B. Should we show a red tag for UNAPPROVABLE and show a negative tag?
- *    This was not included in the design specs.
- */
-const getTagDisplayData = approvalState => {
+const getApprovalStateIcon = approvalState => {
     switch (approvalState) {
-        case 'APPROVED_HERE':
-        case 'APPROVED_ABOVE':
-            return {
-                icon: Approved,
-                displayName: i18n.t('Approved'),
-                type: 'positive',
-            }
-
+        case 'UNAPPROVED_READY':
         case 'ACCEPTED_HERE':
             return {
                 icon: Ready,
-                displayName: i18n.t('Ready for approval and accepted'),
                 type: 'neutral',
             }
-
-        case 'UNAPPROVED_READY':
-            return {
-                icon: Ready,
-                displayName: i18n.t('Ready for approval'),
-                type: 'neutral',
-            }
-
         case 'UNAPPROVED_WAITING':
         case 'UNAPPROVED_ABOVE':
             return {
                 icon: Waiting,
-                displayName: i18n.t('Waiting'),
                 type: 'default',
             }
-
+        case 'APPROVED_HERE':
+        case 'APPROVED_ABOVE':
+            return {
+                icon: Approved,
+                type: 'positive',
+            }
         case 'UNAPPROVABLE':
             return {
-                icon: Waiting,
-                displayName: i18n.t('Cannot approve'),
+                icon: IconBlock16,
                 type: 'negative',
             }
-
+        case 'ERROR':
+            return {
+                icon: IconError16,
+                type: 'negative',
+            }
         default:
             throw new Error(`Unknown approval state: '${approvalState}'`)
     }
 }
 
-export { getTagDisplayData }
+const getApprovalStateText = approvalState => {
+    switch (approvalState) {
+        case 'UNAPPROVED_READY':
+            return i18n.t('Ready for approval')
+        case 'ACCEPTED_HERE':
+            return i18n.t('Ready for approval — Accepted')
+        case 'UNAPPROVED_WAITING':
+            return i18n.t('Waiting for lower level approval')
+        case 'UNAPPROVED_ABOVE':
+            return i18n.t('Waiting for higher level approval')
+        case 'APPROVED_HERE':
+        case 'APPROVED_ABOVE':
+            return i18n.t('Approved')
+        case 'UNAPPROVABLE':
+            return i18n.t('Cannot be approved')
+        case 'ERROR':
+            return i18n.t('Could not retrieve approval status')
+        default:
+            throw new Error(`Unknown approval state: '${approvalState}'`)
+    }
+}
+
+const getTagDisplayData = approvalState => {
+    const displayName = getApprovalStateText(approvalState)
+    const { icon, type } = getApprovalStateIcon(approvalState)
+
+    return { displayName, icon, type }
+}
+
+export { getTagDisplayData, getApprovalStateIcon }
