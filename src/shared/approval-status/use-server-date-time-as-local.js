2d62a60a4d1cc31e7552837d34c9f22cb7d889a5
Hendrik de Graaf <hendrik@dhis2.org>
2021-09-27 12:40:18 +0200

    fix(approval-status-tag): fix rounding in timezone offset calculation

--- src/shared/approval-status/use-server-date-time-as-local.js
+++ src/shared/approval-status/use-server-date-time-as-local.js
@@ -1,15 +1,18 @@
 import { useConfig } from '@dhis2/app-runtime'
 
+const msPerHr = 1000 * 60 * 60
+
 export const useServerDateTimeAsLocal = dateTime => {
     const { systemInfo } = useConfig()
-    const timestamp = new Date(dateTime).getTime()
     const localNow = new Date()
     const nowAtServerTimeZone = new Date(
         localNow.toLocaleString('en-US', {
             timeZone: systemInfo.serverTimeZoneId,
         })
     )
+    const timestamp = new Date(dateTime).getTime()
     const timeOffset = localNow.getTime() - nowAtServerTimeZone.getTime()
+    const timeOffsetRoundedToHours = Math.round(timeOffset / msPerHr) * msPerHr
 
-    return new Date(timestamp + timeOffset)
+    return new Date(timestamp + timeOffsetRoundedToHours)
 }
