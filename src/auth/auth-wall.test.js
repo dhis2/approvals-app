529e81558b895f7cfc7719a3c914557a0ce2e9d4
Hendrik de Graaf <hendrik@dhis2.org>
2021-09-30 17:37:45 +0200

    chore: revert "show message to users with no authority to approve data"
    This reverts commit 1462f6d09d31a5bda28d50e2209b88e6ae1deb1b.

--- src/auth/auth-wall.test.js
+++ src/auth/auth-wall.test.js
@@ -14,38 +14,15 @@ afterEach(() => {
 
 describe('<AuthWall>', () => {
     it('shows a noticebox for unauthorized users', () => {
-        useIsAuthorized.mockImplementation(() => ({
-            hasAppAccess: false,
-            hasApprovalAuthorities: false,
-        }))
+        useIsAuthorized.mockImplementation(() => false)
 
         const wrapper = shallow(<AuthWall>Child</AuthWall>)
 
         expect(wrapper.find(ErrorMessage)).toHaveLength(1)
-        expect(wrapper.prop('children')).toBe(
-            "You don't have access to the Data Approval App. Contact a system administrator to request access."
-        )
-    })
-
-    it('shows a noticebox for users without appropriate authorities', () => {
-        useIsAuthorized.mockImplementation(() => ({
-            hasAppAccess: true,
-            hasApprovalAuthorities: false,
-        }))
-
-        const wrapper = shallow(<AuthWall>Child</AuthWall>)
-
-        expect(wrapper.find(ErrorMessage)).toHaveLength(1)
-        expect(wrapper.prop('children')).toBe(
-            'You are not allowed to approve data. Contact a system administrator to request the appropriate authorities.'
-        )
     })
 
     it('renders the children for authorised users', () => {
-        useIsAuthorized.mockImplementation(() => ({
-            hasAppAccess: true,
-            hasApprovalAuthorities: true,
-        }))
+        useIsAuthorized.mockImplementation(() => true)
 
         const wrapper = shallow(<AuthWall>Child</AuthWall>)
 
--- src/auth/use-is-authorized.js
+++ src/auth/use-is-authorized.js
@@ -2,15 +2,7 @@ import { useAppContext } from '../app-context/index.js'
 
 export const useIsAuthorized = () => {
     const { authorities } = useAppContext()
-    const hasAppAccess = authorities.some(
+    return authorities.some(
         authority => authority === 'ALL' || authority === 'M_dhis-web-approval'
     )
-    const hasApprovalAuthorities = authorities.some(
-        authority =>
-            authority === 'ALL' ||
-            authority === 'F_APPROVE_DATA' ||
-            authority === 'F_APPROVE_DATA_LOWER_LEVELS'
-    )
-
-    return { hasAppAccess, hasApprovalAuthorities }
 }
