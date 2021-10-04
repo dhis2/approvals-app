3ab68198fb651e635b14825ad63032d14b0f2f63
Hendrik de Graaf <HendrikThePendric@users.noreply.github.com>
2021-10-04 15:03:42 +0200

    test(approval-status-tag): add test for new approval state (#107)
    * test(approval-status-tag): add test for new
    
    * fix(approval-status-tag): adjust failing test

--- src/shared/approval-status/get-approval-status.test.js
+++ src/shared/approval-status/get-approval-status.test.js
@@ -83,6 +83,17 @@ describe('getApprovalStatusDisplayData', () => {
             type: 'positive',
         })
     })
+    it('returns the correct display data for approval status "UNAUTHORIZED"', () => {
+        expect(
+            getApprovalStatusDisplayData({
+                approvalStatus: APPROVAL_STATUSES.UNAUTHORIZED,
+            })
+        ).toEqual({
+            displayName: 'You do not have authority to approve data',
+            icon: IconBlock16,
+            type: 'negative',
+        })
+    })
     it('returns the correct display data for approval status "UNAPPROVABLE"', () => {
         expect(
             getApprovalStatusDisplayData({
